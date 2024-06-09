70678def0030adebea8a74203074715bfff1906a
ddddddd1888 <aidenjcheng12@gmail.com>
2024-06-09 10:35:29 -0400

    added testimony marquee, finished bento box section.

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ShinyText from "./shinytext.tsx";
+import Bento from "./bento.tsx";
 
 import "./index.css";
 
 const rootElement = document.getElementById("root");
 const aibubbleElement = document.getElementById("aibubble-container");
-
+const bento = document.getElementById("bento");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
@@ -19,3 +20,5 @@ if (rootElement) {
 
 const root = createRoot(aibubbleElement!);
 root.render(<ShinyText />);
+const root2 = createRoot(bento!);
+root2.render(<Bento />);
